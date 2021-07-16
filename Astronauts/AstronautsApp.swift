8218e85faea352185b2c72bf932417704f222dc5
Eduardo Dias <eduardo.dias@iag.co.nz>
2021-07-17 00:58:42 +1200

    Add initial project files

---
+++ Astronauts/AstronautsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronautsApp.swift
+//  Astronauts
+//
+//  Created by Eduardo Dias on 17/07/21.
+//
+
+import SwiftUI
+
+@main
+struct AstronautsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Astronauts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Astronauts
+//
+//  Created by Eduardo Dias on 17/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
